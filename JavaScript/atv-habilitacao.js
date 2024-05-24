e8b114ffcae36333c46b7cff4ca9b1fade5df242
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-24 12:03:42 -0300

    Add files via upload

---
+++ JavaScript/atv-habilitacao.js
@@ -0,0 +1,11 @@
+
+alert('Quer saber se pode tirar hablitação?');
+
+let idade = prompt('Informe sua idade:');
+
+if(idade<18){
+    alert('Menores de idade não podem tirar hablitação.')
+}
+else{
+    alert('Você pode tirar hablitação.')
+}
